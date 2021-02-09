050478dd0b4850890cd701f8f58d84997c76f78b
ooeeda <ooeeda@gmail.com>
2021-02-09 22:17:08 +0900

    MAP 활용해보기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
+const foodILike = [
+        {
+            name : "볶음밥",
+            image : "https://t1.daumcdn.net/cfile/tistory/23544B3E51C4081608"
+
+        },
+        {
+            name : "라면",
+            image : "https://i.ytimg.com/vi/e1ADIzNXK9g/maxresdefault.jpg"
+
+        },
+        {
+            name : "햄버거",
+            image : "https://img.insight.co.kr/static/2018/07/05/700/bs8obfmy6x14j1301vo3.jpg"
+
+        },
+        {
+            name : "피자",
+            image : "http://cfile23.uf.tistory.com/image/222E3D4F530DEC511A93FD"
+
+        }
+    ];
 /*
 function Food(props){
     //console.log(props.fav);
@@ -7,20 +29,23 @@ function Food(props){
 }*/
 
 //ES6 사용시 {}안에 속성의 이름을 이용해 사용할수 있다.
-function Food({fav}){
+function Food({name, picture}){
 
-    return <h1>I like {fav}</h1>
+    return (
+        <div>
+            <h2>I like {name}</h2>
+            <img src={picture} />
+        </div>
+    );
 }
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!!!</h1>
-        <Food fav="kimchi"/>
-        <Food fav="라면"/>
-        <Food fav="삼겹살"/>
-        <Food fav="쭈꾸미"/>
 
+        {foodILike.map(dish => (
+            <Food name={dish.name} picture={dish.image} />
+            ))}
     </div>
 
   );
