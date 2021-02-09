3ff67bc7ef0ac8d798c3323318717de6b60d97a1
ooeeda <ooeeda@gmail.com>
2021-02-09 17:34:53 +0900

    속성전달

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+/*
+function Food(props){
+    //console.log(props.fav);
+    return <h1>I like {props.fav}</h1>
+}*/
+
+//ES6 사용시 {}안에 속성의 이름을 이용해 사용할수 있다.
+function Food({fav}){
+
+    return <h1>I like {fav}</h1>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello!!!</h1>
+        <Food fav="kimchi"/>
+        <Food fav="라면"/>
+        <Food fav="삼겹살"/>
+        <Food fav="쭈꾸미"/>
+
     </div>
+
   );
 }
 
