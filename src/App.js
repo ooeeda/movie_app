5d0fcc711ff7d01afb0bb0548c730ed4d9a63ccf
ooeeda <ooeeda@gmail.com>
2021-02-09 22:32:29 +0900

    MAP 활용해보기를 람다식에서 펑션으로 변경

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import React from 'react';
 
 const foodILike = [
         {
+            id : 1,
             name : "볶음밥",
             image : "https://t1.daumcdn.net/cfile/tistory/23544B3E51C4081608"
 
         },
         {
+            id : 2,
             name : "라면",
             image : "https://i.ytimg.com/vi/e1ADIzNXK9g/maxresdefault.jpg"
 
         },
         {
+            id : 3,
             name : "햄버거",
             image : "https://img.insight.co.kr/static/2018/07/05/700/bs8obfmy6x14j1301vo3.jpg"
 
         },
         {
+            id : 4,
             name : "피자",
             image : "http://cfile23.uf.tistory.com/image/222E3D4F530DEC511A93FD"
 
@@ -34,18 +38,32 @@ function Food({name, picture}){
     return (
         <div>
             <h2>I like {name}</h2>
-            <img src={picture} />
+            <img src={picture} alt={name}/>
         </div>
     );
+}
+function renderFood(dish){
+
+    //console.log(dish);
+
+    return (
+        <Food key={dish.name} name={dish.name} picture={dish.image}/>
+    );
+
 }
 
 function App() {
   return (
     <div className="App">
 
-        {foodILike.map(dish => (
+  {/*      {foodILike.map(dish => (
             <Food name={dish.name} picture={dish.image} />
-            ))}
+            ))}*/}
+
+        {foodILike.map(renderFood)}
+
+
+
     </div>
 
   );
